187552c7af7de321cc78ad923b3f96cff2f516e8
cartolari <bruno.cartolari@gmail.com>
2015-01-08 01:36:54 -0200

    Change ansible install method

--- dependencies/install_ansible.sh
+++ dependencies/install_ansible.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
-apt-get install software-properties-common -qy
-apt-add-repository ppa:ansible/ansible
-apt-get update
-apt-get install ansible -qy
+apt-get install python-pip -qy
+pip install ansible
