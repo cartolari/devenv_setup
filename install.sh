5f9fecdd1057ca3ff7b3d33b392ce55a225d926e
cartolari <bruno.cartolari@gmail.com>
2015-01-19 01:56:00 -0200

    Change ruby version manager from rbenv to chruby

--- install.sh
+++ install.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 
+set -e
+
 install_ansible() {
-  apt-get install -qy python-dev python-pip
-  pip install ansible
+  apt-get install -y software-properties-common
+  apt-add-repository -y ppa:ansible/ansible
+  apt-get update
+  apt-get install -y ansible
 }
 
 setup_devenv() {
@@ -13,7 +17,12 @@ ansible_installed() {
   command -v ansible > /dev/null
 }
 
+install_dependencies() {
+  ansible-galaxy install -f -r requirements.txt -p roles
+}
+
 if ! ansible_installed; then
   install_ansible
 fi
+install_dependencies
 setup_devenv
